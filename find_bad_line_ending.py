faa435f7303cf82295c60d62b903c462cfc740e4
drupchen <hhdrupchen@gmail.com>
2019-03-11 23:03:48 +0100

    list bad line endings

---
+++ find_bad_line_ending.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+def extract_all(csv_dump):
+    lines = csv_dump.strip().split('\n')  # cut dump in lines
+    lines = [l.split(',')[1] for l in lines]  # only keep the text
+    return lines
+
+
+def has_bad_ending(line):
+    if line and line.strip()[-1] not in '་།ཿ༔':
+        return True
+    return False
+
+
+bad_lines = []
+for f in Path('to-check/tengyur_last_volumes/').glob('*.csv'):
+    lines = extract_all(f.read_text())
+    for num, l in enumerate(lines):
+        if has_bad_ending(l):
+            bad_lines.append((f.stem, num, l))
+
+out = '\n'.join([','.join([str(x) for x in a]) for a in bad_lines])
+Path('bad_line_endings.csv').write_text(out)
